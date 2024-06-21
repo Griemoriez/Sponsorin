a04be30ee7929e35d09e6bfb9a47be52d4d4a2e0
spaceJump13 <108264170+spaceJump13@users.noreply.github.com>
2024-06-21 19:04:35 +0700

    commit

--- flut_sponsorin/lib/company_view/upcoming_events.dart
+++ flut_sponsorin/lib/company_view/upcoming_events.dart
@@ -1,3 +1,5 @@
+import 'package:flut_sponsorin/company_view/list_overall_events.dart';
+import 'package:flut_sponsorin/sponsor_seeker_view/list_company.dart';
 import 'package:flutter/material.dart';
 
 class upcoming_events extends StatelessWidget {
@@ -5,13 +7,68 @@ class upcoming_events extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-            child: Text(
-              'upcoming Event',
-              style: TextStyle(fontSize: 24, color: Colors.black),
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 15.0, left: 15.0, right: 15.0, bottom: 18.0),
+            child: SizedBox(
+              height: 45.0,
+              width: double.infinity,
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search),
+                  hintText: 'Search here...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 10.0, left: 20.0, right: 15.0, bottom: 3.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Upcoming Events',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Builder(builder: (context) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        // Use Navigator.push for navigation
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const list_overall_events()),
+                      );
+                    },
+                    child: Text(
+                      'All Events',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.cyan[300],
+                      ),
+                    ),
+                  );
+                }),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
